3bd4bb58ab7714fa7fd58cbd137a99c51f3a087d
Abil Hakim <ablhkm.dev@gmail.com>
2023-07-28 08:41:01 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -5,10 +5,9 @@ namespace LibazTech\Belajar;
 class Customer {
     public function __construct(private string $name)
     {
-        
     }
 
-    public function SayHello(string $name)
+    public function SayHello(string $name = "Guest"): string
     {
         return "Hello $name, My name is $this->name";
     }
